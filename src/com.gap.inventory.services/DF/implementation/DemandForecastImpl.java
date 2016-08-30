3a05a1343f673064abab5d520cbbe26229d6e1ec
Harshad <harsh.patki@gmail.com>
2016-08-30 19:37:41 +1000

    code changes to implementation files

--- src/com.gap.inventory.services/DF/implementation/DemandForecastImpl.java
+++ src/com.gap.inventory.services/DF/implementation/DemandForecastImpl.java
@@ -3,21 +3,26 @@ package com.gap.inventory.services.DF.implementation;
 import com.gap.inventory.services.DF.domain.DemandForecast;
 import com.gap.inventory.services.DF.intrface.IDemandForecast;
 import com.gap.inventory.services.common.ApplicationContextUtil;
+import com.gap.inventory.services.common.RestService;
 
 /**
  * Created by Hpatki on 8/29/2016.
  */
 class DemandForecastImpl implements IDemandForecast
 {
+    private RestService endPoint;
+
     public DemandForecastImpl ()
     {
-
+        String baseURL = ApplicationContextUtil.getProperty ("baseurl");
+        endPoint = new RestService(baseURL);
     }
 
     @Override
     public DemandForecast getForecast (String ccNbr)
     {
-        ApplicationContextUtil.getProperty ("ccforecast");
-           return null;
+        String rSource = ApplicationContextUtil.getProperty ("ccforecast");
+        //endPoint.getObjectUsingGetMethod(rSource,);
+        return null;
     }
 }
--- src/com.gap.inventory.services/common/Main.java
+++ src/com.gap.inventory.services/common/Main.java
@@ -1,6 +1,12 @@
 package com.gap.inventory.services.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.ExpectedCount;
+import org.springframework.web.context.request.WebRequest;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -28,7 +34,7 @@ public class Main implements Serializable
 
     public static void main (String[] args) throws Exception
     {
-        Map<String,String> vals = new HashMap<>();
+        /*Map<String,String> vals = new HashMap<>();
         vals.put("brand","4");
         vals.put("market","JPN");
         vals.put("channel","RTL");
@@ -38,6 +44,20 @@ public class Main implements Serializable
         List<String> ret = inst.getCollectionUsingPostMethod("/allocation/in-season/allocated-customer-choices/search",vals,String.class);
 
         RestService df = new RestService("https://demand-forecast-service-dev.cf.rc.gid.gap.com");
-      //  inst.getObjectUsingGetMethod("/analytics/demand-forecast/customer-choice-by-weeks/{cc-id}?brandId={brd-id}&marketId={mkt-id}&channelId={chnl-id}");
+        inst.getObjectUsingGetMethod("/analytics/demand-forecast/customer-choice-by-weeks/{cc-id}?brandId={brd-id}&marketId={mkt-id}&channelId={chnl-id}");
+        */
+
+        RestTemplate restTemplate = new RestTemplate();
+        MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+
+        //server.expect(ExpectedCount.manyTimes(), requestTo("/hotels/42")).andExpect(method(HttpMethod.GET))
+        //        .andRespond(withSuccess("{ \"id\" : \"42\", \"name\" : \"Holiday Inn\"}", MediaType.APPLICATION_JSON));
+
+        //Hotel hotel = restTemplate.getForObject("/hotels/{id}", Hotel.class, 42);
+        // Use the hotel instance...
+
+        // Verify all expectations met
+        server.verify();
+
     }
 }
