c53ab35d6d2bdf8e119079dfec89b2fcd05cad72
Harshad <harsh.patki@gmail.com>
2016-08-30 10:13:15 +1000

    added code to RestService.java'

--- src/com.gap.inventory.services/common/Main.java
+++ src/com.gap.inventory.services/common/Main.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,16 +28,13 @@ public class Main implements Serializable
 
     public static void main (String[] args) throws Exception
     {
-        List<Main> objects = new ArrayList<Main>();
-        for (int i=0; i<1; ++i) {
-            Main inst = new Main();
-            inst.a = i+1;
-            inst.b = "Harshad" + Integer.toString(i+1);
-            objects.add(inst);
-        }
-
-        ObjectMapper mapper = new ObjectMapper();
-        String str = mapper.writeValueAsString(objects);
-
+        Map<String,String> vals = new HashMap<>();
+        vals.put("brand","4");
+        vals.put("market","JPN");
+        vals.put("channel","RTL");
+        vals.put("level","CC");
+        vals.put("code","161054006");
+        RestService inst = new RestService("https://allocation-catalog-service-dev.cf.rc.gid.gap.com");
+        List<String> ret = inst.getCollectionUsingPostMethod("/allocation/in-season/allocated-customer-choices/search",vals,String.class);
     }
 }
--- src/com.gap.inventory.services/common/RestService.java
+++ src/com.gap.inventory.services/common/RestService.java
@@ -1,11 +1,15 @@
 package com.gap.inventory.services.common;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jndi.toolkit.url.Uri;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Hpatki on 8/29/2016.
@@ -21,28 +25,69 @@ public class RestService
         rstTmpl = new RestTemplate ();
     }
 
-    public <T> T getUsingPostMethod (String resource, Object objects, Class<T> type)
+    public <T> T getObjectUsingPostMethod (String resource, Object objects, Class<T> type)
     {
-        Object objRet = null;
+        String response = invokeHTTP(resource,objects);
+        ObjectMapper obj = new ObjectMapper();
+        Object objRef = null;
+        try
+        {
+            objRef = obj.readValue(response, type);
+        }
+        catch(Exception err)
+        {
+            objRef = null;
+        }
+
+        return (T)objRef;
+    }
+
+    public <T> List<T> getCollectionUsingPostMethod (String resource, Object objects, Class<T> type)
+    {
+        String response = invokeHTTP(resource, objects);
+        ObjectMapper obj = new ObjectMapper();
+        Object objRef = null;
+        try
+        {
+            objRef = obj.readValue(response, new TypeReference<List<T>>() {});
+        }
+        catch(Exception err)
+        {
+
+        }
+        return (List<T>)objRef;
+    }
+
+    public <T> T getObjectUsingGetMethod (String resource, Class<T> type)
+    {
+         return null;
+    }
+
+    public <T> T getCollectionUsingGetMethod (String resource, Class<T> type)
+    {
+         return null;
+    }
+
+    private String invokeHTTP (String resource, Object objects)
+    {
+        String resp = null;
 
         HttpHeaders hdr = new HttpHeaders();
         hdr.setContentType(MediaType.APPLICATION_JSON);
         ObjectMapper obj = new ObjectMapper();
-        String str = null;
+
         try
         {
-            str = obj.writeValueAsString(objects);
+            String str = obj.writeValueAsString(objects);
             HttpEntity<String> entity = new HttpEntity<String>(str, hdr);
-            String resp = rstTmpl.postForObject(baseURL+resource,entity, String.class);
-
-            objRet = obj.readValue (resp, type);
+            resp = rstTmpl.postForObject(baseURL+resource,entity, String.class);
         }
         catch(Exception err)
         {
             //log a message
-            objRet = null;
+            resp = null;
         }
 
-        return (T)objRet;
+        return resp;
     }
 }
